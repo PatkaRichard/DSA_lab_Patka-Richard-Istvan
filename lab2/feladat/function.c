2f36b2cc8abc26e283a0aa533e344b59c837179d
PatkaRichard <patka.richard@student.ms.sapientia.ro>
2021-02-23 15:30:56 +0200

    feladat

---
+++ lab2/feladat/function.c
@@ -0,0 +1,8 @@
+//
+// Created by patka on 23/02/2021.
+//
+
+#include "function.h"
+sum(int a, int b){
+    return a+b;
+}
---
+++ lab2/feladat/function.h
@@ -0,0 +1,8 @@
+//
+// Created by patka on 23/02/2021.
+//
+
+#ifndef FELADAT_FUNCTION_H
+#define FELADAT_FUNCTION_H
+int sum(int a, int b);
+#endif //FELADAT_FUNCTION_H
