0676d1d25fba3dfcfe1c258cbcf3bce6a76a5c35
PatkaRichard <patka.richard@student.ms.sapientia.ro>
2021-03-09 16:08:49 +0200

    Labor 4

---
+++ lab4/feladat1/function.h
@@ -0,0 +1,22 @@
+//
+// Created by patka on 09/03/2021.
+//
+
+#ifndef FELADAT1_FUNCTION_H
+#define FELADAT1_FUNCTION_H
+typedef struct  {
+    int rows; // sorok száma
+    int cols; // oszlopok száma
+    int **data; //
+}Matrix;
+Matrix* createMatrix(int rows, int cols);
+
+//tömb tartalmának beolvasása állományból
+void readMatrix(const char *fileName, Matrix **array);
+
+
+//tömb tartalmának kiiratása a képernyőre
+void printMatrix(Matrix* array);
+
+
+#endif //FELADAT1_FUNCTION_H
